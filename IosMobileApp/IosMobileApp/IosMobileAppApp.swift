dbae70d11292cc20c8177208be9eb344f6a5d91a
Sd4a <jasnjoku@gmail.com>
2025-02-08 16:16:28 +0000

    Add files via upload

---
+++ IosMobileApp/IosMobileApp/IosMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosMobileAppApp.swift
+//  IosMobileApp
+//
+//  Created by Student on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IosMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IosMobileApp/IosMobileAppTests/IosMobileAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IosMobileAppTests.swift
+//  IosMobileAppTests
+//
+//  Created by Student on 14/03/2025.
+//
+
+import Testing
+@testable import IosMobileApp
+
+struct IosMobileAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
