c0ae85b503b1dbb99fb7fadfd170e608fd6f6864
rod <rodion7771@gmail.com>
2025-03-28 12:43:02 +0000

    Created profile View

--- IosMobileApp/IosMobileApp/Views/ProfileView.swift
+++ IosMobileApp/IosMobileApp/Views/ProfileView.swift
@@ -1,18 +1,51 @@
-//
-//  ProfileView.swift
-//  IosMobileApp
-//
-//  Created by Student on 28/03/2025.
-//
-
 import SwiftUI
 
-struct ProfileView: View {
+struct ProfileSettingsView: View {
+    @State private var notificationsEnabled = false
+    @State private var dataSharingEnabled = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading) {
+                // Profile Picture
+                HStack {
+                    Image("profile_pic") // Replace with your image name
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .padding()
+                    
+                    VStack(alignment: .leading) {
+                        Text("User Name")
+                            .font(.headline)
+                        Text("user@example.com")
+                            .font(.subheadline)
+                    }
+                    .padding(.leading)
+                }
+
+                // Notifications Toggle
+                Toggle(isOn: $notificationsEnabled) {
+                    Text("Enable Notifications")
+                }
+                .padding()
+                
+                // Data Sharing Toggle
+                Toggle(isOn: $dataSharingEnabled) {
+                    Text("Share Data")
+                }
+                .padding()
+                
+                Spacer()
+            }
+            .navigationTitle("Profile Settings")
+        }
     }
 }
 
-#Preview {
-    ProfileView()
+struct ProfileSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSettingsView()
+    }
 }
