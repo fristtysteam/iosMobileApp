fcb0da53118ed2af206519acaedda492da645c58
Kosy <kosysteve03@gmail.com>
2025-03-28 12:05:42 +0000

    update

---
+++ IosMobileApp/IosMobileApp/Views/Components/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  IosMobileApp
+//
+//  Created by Student on 28/03/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
--- IosMobileApp/IosMobileApp/Views/ProfileView.swift
+++ IosMobileApp/IosMobileApp/Views/ProfileView.swift
@@ -1,18 +1,18 @@
 //
-//  addGoalPage.swift
+//  ProfileView.swift
 //  IosMobileApp
 //
-//  Created by Student on 24/03/2025.
+//  Created by Student on 28/03/2025.
 //
 
 import SwiftUI
 
-struct addGoalPage: View {
+struct ProfileView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    addGoalPage()
+    ProfileView()
 }
