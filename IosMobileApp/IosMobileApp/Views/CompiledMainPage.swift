e7d8baff6e2ede5d9938d911c3d2e9ee469520b0
Marco <D00250937@student.dkit.ie>
2025-04-14 11:29:20 +0100

    Jason - Updated Pages

---
+++ IosMobileApp/IosMobileApp/Views/CompiledMainPage.swift
@@ -0,0 +1,7 @@
+//
+//  CompiledMainPage.swift
+//  IosMobileApp
+//
+//  Created by Student on 11/04/2025.
+//
+
---
+++ IosMobileApp/IosMobileApp/Views/GoalConnectPage.swift
@@ -0,0 +1,18 @@
+//
+//  GoalConnectPage.swift
+//  IosMobileApp
+//
+//  Created by Student on 11/04/2025.
+//
+
+import SwiftUI
+
+struct GoalConnectPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalConnectPage()
+}
